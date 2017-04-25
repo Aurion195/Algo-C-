0c8ebc0b68fd3a8a827d57a2a84f43f160470a34
Thomas Mathieu <mathieuthomas84300@hotmail.fr>
2017-04-25 16:45:23 +0200

    Add files via upload

--- TP 2/TP_boule.cpp
+++ TP 2/TP_boule.cpp
@@ -7,7 +7,7 @@ int  * question(int N, int x, int d)
 
 {
   int *Tbis=new int [N] ;
-  int poid=10 ;
+  int poid=100 ;
 
   for(int i=1 ; i<=N ; i++)
   {
@@ -31,58 +31,90 @@ int balance(int *Tab, int taille, int d1, int d2)
 
 int resoudre(int* Tab, int taille, int d1, int d2, int d3,int& plus_moins,int& cpt)
 {
-  int somme_1=balance(Tab,taille,d1,d2) ;
-  cout << "Pesée : " << cpt << endl ;
-  cpt++ ;
-
   if(taille==1)
   {
     if(plus_moins!=0)
     {
+      int somme_1=balance(Tab,taille,d1,d2) ;
+      //cout << "Pesée : " << cpt << endl ;
+      cpt++ ;
+
       if(somme_1==0) { return d3 ; }
       else if((somme_1>0 && plus_moins>0) || (somme_1<0 && plus_moins<0)) { return d1 ; }
       else if((somme_1<0 && plus_moins>0) || (somme_1>0 && plus_moins<0)) { return d2 ; }
     }
     else
     {
-      int somme_2=balance(Tab,taille,d1,d3) ;
-      cout << "Pesée : " << cpt << endl ;
+      int somme_1=balance(Tab,taille,d1,d2) ;
+      //cout << "Pesée : " << cpt << endl ;
       cpt++ ;
-      
-      if((somme_1<0 && somme_2==0) || (somme_1<0 && somme_2<0)) {plus_moins=-1 ;}
-      else {plus_moins=1 ;}
 
-      if(somme_1==0) { return d3 ; }
-      else if((somme_1>0 && somme_2>0 && plus_moins>0) || (somme_1<0 && somme_2>0 && plus_moins<0)) { return d1; }
-      else if((somme_1<0 && somme_2==0 && plus_moins<0) || (somme_1>0 && somme_2==0 && plus_moins>0)) { return d2 ; }
+      int somme_3=balance(Tab,taille,d1,d3) ;
+      //cout << "Pesée : " << cpt << endl ;
+      cpt++ ;
+
+      if(somme_1==0)
+      {
+        if(somme_3<0) {plus_moins=1 ;}
+        else {plus_moins=-1 ;}
+        return d3 ;
+      }
+      else if((somme_1>0 && somme_3>0) || (somme_1<0 && somme_3<0))
+      {
+        if(somme_1>0) {plus_moins=-1 ;}
+        else {plus_moins=1 ;}
+        return d1;
+      }
+      else if((somme_1>0 && somme_3==0) || (somme_1<0 && somme_3==0))
+      {
+        if(somme_1>0) {plus_moins=1 ;}
+        else {plus_moins=-1 ;}
+        return d2 ;
+      }
     }
   }
   else
   {
+    int somme_1=balance(Tab,taille,d1,d2) ;
+    //cout << "Pesée : " << cpt << endl ;
+    cpt++ ;
+
     if(plus_moins!=0)
     {
-      if(somme_1==0) { return resoudre(Tab, taille/3, d3, d3+(taille/3), d3+(taille/3*2), plus_moins, cpt) ; }
-      else if((somme_1<0 && plus_moins<0) || (somme_1>0 && plus_moins>0)) { return resoudre(Tab, taille/3, d1, d1+taille/3, d1+(taille/3*2), plus_moins, cpt) ; }
-      else { return resoudre(Tab, taille/3, d2, d2+taille/3, d2+(taille/3*2), plus_moins, cpt) ; }
+      if(somme_1==0) {return resoudre(Tab,taille/3,d3,d3+taille/3,d3+(2*taille/3),plus_moins,cpt) ;}
+      else if((somme_1<0 && plus_moins>0) || (somme_1>0 && plus_moins<0)) {return resoudre(Tab,taille/3,d2,d2+taille/3,d2+(2*taille/3),plus_moins,cpt) ;}
+      else {return resoudre(Tab, taille/3, d1, d1+taille/3, d1+(2*taille/3), plus_moins, cpt) ; }
     }
 
     int somme_2=balance(Tab,taille,d1,d3) ;
-    cout << "Pesée : " << cpt << endl ;
+    //cout << "Pesée : " << cpt << endl ;
     cpt++;
 
-    if((somme_1<0 && somme_2==0) || (somme_1<0 && somme_2<0)) {plus_moins=-1 ;}
-    else {plus_moins=1 ;}
-
-    if(somme_1==0) {return resoudre(Tab,taille,d3,d3+taille/3,d3+(2*taille/3),plus_moins,cpt) ;}
-    else if ((somme_1>0 && somme_2>0) || (somme_1<0 && somme_2<0)) { return resoudre(Tab, taille/3, d1, d1+taille/3, d1+(2*taille/3), plus_moins, cpt) ; }
-    else if((somme_1>0 && somme_2==0) || (somme_1<0 && somme_2==0)) { return resoudre(Tab, taille/3, d2, d2+taille/3, d2+(2*taille/3*2), plus_moins, cpt) ; }
+    if(somme_1==0)
+    {
+      if(somme_2<0) {plus_moins=1 ;}
+      else {plus_moins=-1 ;}
+      return resoudre(Tab,taille/3,d3,d3+taille/3,d3+(2*taille/3),plus_moins,cpt) ;
+    }
+    else if((somme_1>0 && somme_2==0) || (somme_1<0 && somme_2==0))
+    {
+      if(somme_1>0) {plus_moins=-1 ;}
+      else {plus_moins=1 ;}
+      return resoudre(Tab, taille/3, d2, d2+taille/3, d2+(2*taille/3), plus_moins, cpt) ;
+    }
+    else
+    {
+      if(somme_1>0) {plus_moins=1 ;}
+      else {plus_moins=-1 ;}
+      return resoudre(Tab, taille/3, d1, d1+taille/3, d1+(2*taille/3), plus_moins, cpt) ;
+    }
   }
 }
 
-
-bool verifier(int* Tab, int N, int y, int pm)
+bool verifier(int* Tab, int taille, int y, int x, int pm)
 {
-  for(int i=1 ; i<=N ;i++)
+  if(x!=y) return false ;
+  for(int i=1 ; i<taille ;i++)
   {
     if(pm>0) { if(Tab[y]>Tab[i]) {return true ;} }
     else if (pm<0) { if(Tab[y]<Tab[i]) {return true ;} }
@@ -100,24 +132,63 @@ void affichage(int *Tab, int N)
   }
 }
 
+bool valider(int taille)
+{
+	for(int i=1 ; i<=taille ; i++)
+	{
+    int cpt=1 ;
+		int plus_moins=0 ;
+		int * T = question(taille, i, 1);
+		int y=resoudre(T, taille/3, 1, 1+taille/3, 1+(2*taille/3), plus_moins,cpt);
+    int pm=plus_moins ;
+
+		if(!verifier(T, taille, y, i, pm))
+		{
+			cout << "Erreur pour : " << " (" << i << ") +" << endl;
+			return false;
+		}
+
+		T=question(taille, i, -1);
+		plus_moins=0;
+		int z=resoudre(T, taille/3, 1, 1+taille/3, 1+(2*taille/3), plus_moins,cpt);
+    int pm_2=plus_moins ;
+
+		if(!verifier(T, taille, z,i, pm_2))
+		{
+			cout << "Erreur pour ; " <<" (" << i << ")" << endl;
+			return false;
+		}
+	}
+
+  cout << "Tout es bon !!! :) Vous pouvez partir du cour !! " << endl ;
+	return true;
+}
+
 int main()
 {
-  bool trouve ;
+  bool trouver ;
 
-  int *Tab=question(27,1,4) ;
-  int plus_moins=1 ;
-  int cpt=1 ;
-  //affichage(Tab,27) ;
+  /*for(int a=1 ; a<28 ; a++)
+  {
+    int *Tab=question(27,a,10) ;
+    int plus_moins=0 ;
+    int cpt=1 ;
+    //affichage(Tab,27) ;
 
-  int y=resoudre(Tab,9,1,10,17,plus_moins,cpt) ;
-  cout << "La boule intruse est la boule : " << y << endl ;
-  if(plus_moins>0) {cout << "La différence de poids est supétieur !   : " << plus_moins << endl ;}
-  else {cout << "La différence de poids est inférieur !   : " << plus_moins << endl ;}
+    cout << "Test de la fonction pour indice : " << a << endl ;
+    int y=resoudre(Tab,9,1,10,19,plus_moins,cpt) ;
+    cout << "La boule intruse est la boule : " << y << endl ;
+    if(plus_moins>0) {cout << "La différence de poids est supétieur !   : " << plus_moins << endl ;}
+    else {cout << "La différence de poids est inférieur !   : " << plus_moins << endl ;}
 
-  int pm=plus_moins ;
-  //trouve=verifier(Tab,17,y,pm) ;
-  //if(trouve) {cout << "La fonction résoudre marche" << endl ;}
-  //else {cout << "La fonctionr résoudre ne marche pas" << endl ;}
+    int pm=plus_moins ;
+    bool trouve=verifier(Tab,27,y,a,pm) ;
+    if(trouve==true) {cout << "fonctionne" << endl ;}
+    else {cout << "erreur" << endl ;}
+    cout << "----------------------------" << "\n" << endl ;
+  }*/
+
+  trouver=valider(27) ;
 
   return 0 ;
 }
